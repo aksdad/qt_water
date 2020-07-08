80975ba311001258c4e9e198a839e6a5f1a5938b
darkyldull <42959586+darkyldull@users.noreply.github.com>
2020-07-08 08:55:50 +0530

    Removed token, adding user  (#2)
    * qwe token
    
    * Adding DM to user(me)
    
    * Adding reaction
    
    * adding emoji
    
    * Removing extra line and removing user ID

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import tasks, commands
 
 token = ""
+emoji = '💦'
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -10,13 +12,18 @@ class MyClient(discord.Client):
         print(self.user.id)
         print('------')
         self.water_printer.start()
-    
+
     @tasks.loop(seconds=10.0)
     async def water_printer(self):
         message_channel = self.get_channel(729573884722151486)
-        print('sending message to channel ' + '729573884722151486: ' + message_channel.name + '\n')
-        await message_channel.send('drink water pls domo arigatou - Sahil \'Darky\' Guleria')
-
+        user = self.get_user()
+        print('sending message to channel ' +
+              '729573884722151486: ' + message_channel.name + '\n')
+        message = await message_channel.send('drink water pls domo arigatou - Sahil \'Darky\' Guleria')
+        await message.add_reaction(emoji)
+        print('sending message to user ' +
+              '240873900307775498: ' + user.name + '\n')
+        await user.send('drinkit')
 
 
 client = MyClient()
